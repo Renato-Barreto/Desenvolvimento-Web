153ac961a7c154c1a5922e5918b1b3b02d0d014a
Renato-Barreto <143633174+Renato-Barreto@users.noreply.github.com>
2024-05-08 16:31:57 -0300

    Add files via upload

---
+++ Filmes(API)/js/script.js
@@ -0,0 +1,37 @@
+const frmPesquisa = document.querySelector("form");
+const apikey = '60706f4d'; 
+
+frmPesquisa.onsubmit = (ev) =>{
+    ev.preventDefault();
+    
+    const pesquisa = ev.target.pesquisa.value;
+
+    if (pesquisa == ""){
+        alert('Preencha o campo');
+        return;
+    }
+
+    fetch(`https://omdbapi.com/?s=${pesquisa}&apikey=${apikey}`)
+        .then(result => result.json())
+        .then(json => carregaLista(json))
+}
+
+const carregaLista = (json) => {
+    const lista = document.querySelector(".lista")
+    lista.innerHTML = "";
+
+    if (json.Response == 'False'){
+        alert('Nenhum filme encontrado');
+        return;
+    } 
+
+    json.Search.forEach(element => {
+        
+        let item = document.createElement("div");
+        item.classList.add("item");
+
+        item.innerHTML = `<img src="${element.Poster}" /><h2>${element.Title}</h2>`
+
+        lista.appendChild(item);
+    });
+}
